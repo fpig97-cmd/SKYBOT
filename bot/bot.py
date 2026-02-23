efd61b58ac41c5768186445c4faaf890386f1485
fpig97-cmd <fpig97@gmail.com>
2026-02-23 20:05:29 +0900

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -21,10 +21,10 @@ TOKEN = str(os.getenv("DISCORD_TOKEN"))
 GUILD_ID = int(os.getenv("GUILD_ID", "0"))
 OWNER_ID = int(os.getenv("OWNER_ID", "0"))
 
-RANK_API_URL_ROOT = os.getenv("RANK_API_URL_ROOT")
+RANK_API_URL_ROOT = https://surprising-perfection-production-7dde.up.railway.app
 RANK_API_KEY = os.getenv("RANK_API_KEY")
 
-CREATOR_ROBLOX_NICK = "DeSky_Lunarx"
+CREATOR_ROBLOX_NICK = "Sky_Lunarx"
 CREATOR_ROBLOX_REAL = "Sky_Lunarx"
 CREATOR_DISCORD_NAME = "Lunar"
 
